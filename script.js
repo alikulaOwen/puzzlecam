0094347b64cb01b1b9a799f8fbb246d31ca11f2e
Alikula Owen <alikulaowen@gmail.com>
2022-02-06 18:17:19 +0300

    fixed drag issue

--- script.js
+++ script.js
@@ -194,6 +194,6 @@ class Piece{
 
 function distance(p1,p2){
     return Math.sqrt(
-        (p1.x-p2.x)*(p2.x-p2.x) + (p1.y-p2.y)*(p2.y-p2.y) 
+        (p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y) 
     )
 }
